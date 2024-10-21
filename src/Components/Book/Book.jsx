273d725e5aa65b0822b662f55f8ed8e80bea468b
Mehedi-m <djm4223@gmail.com>
2024-10-22 00:42:24 +0600

    home design done without functionality

---
+++ src/Components/Book/Book.jsx
@@ -0,0 +1,42 @@
+import { CiStar } from "react-icons/ci";
+
+const Book=({book})=>{
+
+    const{image,bookId,author,bookName,category,tags,rating}=book;
+
+    return(
+
+        <div className="mt-12 ">
+            <div className="card bg-base-100 w-96 border-2 border-gray-300 shadow-xl">
+  <figure className="px-6 pt-6">
+    <img
+      src={image}
+      alt="Shoes"
+      className="rounded-xl h-[230px]" />
+  </figure>
+  <div className="flex gap-2 mx-6 mt-4">
+  {tags.map((tag, index) => (
+    <span key={index} className="badge text-[#23BE0A]">
+      {tag}
+    </span>
+  ))}
+</div>
+  <div className="mx-6 mt-4">
+    <h2 className="card-title text-2xl">{bookName}</h2>
+    <p className="text-gray-500 mt-3 font-semibold">by: {author}</p>
+    <p className="border-b-2 border-dashed border-gray-200  mt-3 mb-3"></p>
+  </div>
+
+  <div className="mx-6 flex justify-between mb-6 font-semibold text-gray-500">
+    <p>{category}</p>
+    <p className="inline-flex justify-center items-center gap-2">{rating} <CiStar/></p>
+
+  </div>
+
+</div>
+
+        </div>
+    )
+}
+
+export default Book;
--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Book from "../Book/Book";
 
 
 const Books=()=>{
+
+    const [Books, setBooks]=useState([]);
+
+    useEffect(()=>{
+
+        fetch("books.json")
+        .then(res=>res.json())
+        .then(data=>setBooks(data))
+
+    },[])
 
     return(
 
@@ -8,9 +21,9 @@ const Books=()=>{
 
             <p className="text-black font-bold text-4xl text-center mx-36 my-20">Books</p>
 
-            <div>
+            <div className="grid grid-cols-3 mx-36 mb-36">
                 {
-                    
+                    Books.map(book=> <Book key={book.id} book={book}></Book>)
                 }
             </div>
 
